7e1207c0e72e5d80c5d1741897d060c21a606b6c
JANSON GAN <jansongan@JANSONGAN-MacBook.local>
2022-04-05 14:15:51 +0800

    TypeScript practice

--- src/screens/dashboard/info/index.tsx
+++ src/screens/dashboard/info/index.tsx
@@ -3,11 +3,17 @@ import { Text, View } from "react-native";
 import styles from "./styles";
 
 const Info: React.FC = () => {
+  const text: string = "a";
+  const digit: number = 123;
 
   return (
     <View style={styles.container}>
       <Text>
-        Info Page: You don't need to login to view this page
+        {/* Info Page: You don't need to login to view this hello */}
+        {text} is a string.
+        </Text>
+      <Text>
+        {digit} is a number.
       </Text>
     </View>
   );
